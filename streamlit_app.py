c43daa7c651705a0417ffcc17f0828d5f3ab87fd
Supriya Kulkarni <103729794+supkulka@users.noreply.github.com>
2022-04-15 12:57:19 +0530

    Update streamlit_app.py
    syntax error resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My mom new healthy dinner')
+streamlit.title('My Mom\'s new healthy dinner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
