db6d45d96280ac13a658fa941daa0006d88ee9eb
Supriya Kulkarni <103729794+supkulka@users.noreply.github.com>
2022-04-15 13:25:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,11 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Banana','Apple'])
+fruits_selected=("pick some fruits:",list(my_fruit_list.index),['Banana','Apple'])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 
 #display table on page 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
 
