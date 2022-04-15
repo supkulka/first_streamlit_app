1a7e814ae85cba53b7416d97eb6615530eb2957e
Supriya Kulkarni <103729794+supkulka@users.noreply.github.com>
2022-04-15 13:18:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,13 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
 
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
+
+#display table on page 
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
-my_fruit_list=my_fruit_list.set_index('Fruit')
+
+
