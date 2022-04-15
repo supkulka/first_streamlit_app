cffb425e7fce0bd98ec7dc8ff675809d2fc5bfc3
Supriya Kulkarni <103729794+supkulka@users.noreply.github.com>
2022-04-15 12:55:54 +0530

    Update streamlit_app.py
    health menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('We are Indians')
+streamlit.title('My mom's new healthy dinner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
