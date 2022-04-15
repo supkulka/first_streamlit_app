9ecbabc8c5322056aac7c058ff492f150171696e
Supriya Kulkarni <103729794+supkulka@users.noreply.github.com>
2022-04-15 13:13:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,4 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 streamlit.dataframe(my_fruit_list)
 
 
-streamlit.multiselect(""pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
